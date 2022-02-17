883003e94b36409ed89c431f4fdfda4b5e7ce494
MatiasBok <matbuken@gmail.com>
2022-02-17 20:16:49 -0300

    Tercer commit: Se agregó carpeta components con NAvBar y Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer>
+        <p>&copy; Copyright 2022 - Matias Bökenhans</p>
+        <p>Todos los derechos reservados</p>
+    </footer>
+  )
+}
+export default Footer
---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <header> 
+            <h1>Mi E-Commerce </h1>  
+            <nav>
+                 <a href="#">cat1</a>   
+                 <a href="#">cat2</a>
+                 <a href="#">cat3</a>       
+            </nav> 
+    </header>
+  )
+}
+
+
+export default NavBar
